c931c2c4bd5e285f24a7a12e76dd4d2660623604
tsshingan <tejasshingan1319@gmail.com>
2024-01-29 16:41:20 +0530

    Even and Odd

---
+++ EvenOdd.cpp
@@ -0,0 +1,37 @@
+/**
+*
+*/
+
+//Code 1:
+#include<iostream>
+using namespace std;
+
+int main(){
+    unsigned int number{0};
+    cout<<"Enter the number:"<<endl;
+    cin>>number;
+    
+    if(number%2==0){
+        cout<<"The "<<number<<" is even";
+    }
+    else{
+        cout<<"The "<<number<<" is odd";
+    }
+    
+}
+
+//Code 2:
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cout<<"Enter the number: ";
+    cin>>number;
+
+    cout << (number % 2 == 0 ? "Even" : "Odd");
+
+    return 0;
+}
+
+
